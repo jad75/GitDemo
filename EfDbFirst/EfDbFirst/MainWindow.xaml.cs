ba4e3017e53345253ebefbb2e2bdc3e856b94132
André <andrerrr@outlook.de>
2019-07-09 16:52:50 +0200

    Ef Db First

---
+++ EfDbFirst/EfDbFirst/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EfDbFirst
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        NorthwindEntities context = null;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            context = new NorthwindEntities();
+
+            grid.ItemsSource = context.Customers.ToList();
+        }
+    }
+}
