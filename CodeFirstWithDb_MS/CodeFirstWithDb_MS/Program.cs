70f74d0e559b431e85f3c96587ceb2e41f34dcd9
André <andrerrr@outlook.de>
2019-07-10 11:52:51 +0200

    CodeFirstWithDb Sackgasse

---
+++ CodeFirstWithDb_MS/CodeFirstWithDb_MS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstWithDb_MS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
