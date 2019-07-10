35dff71b2d939d24cf5ebbda513eaf0a872d92e4
André <andrerrr@outlook.de>
2019-07-10 09:24:26 +0200

    Ef Code First: Model erstellt

---
+++ EfCodeFirst/EfCodeFirst/Model/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCodeFirst.Model
+{
+    public abstract class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime GebDatum { get; set; } = DateTime.Now;
+    }
+
+    public class Kunde : Person
+    {
+        public string KdNummer { get; set; }
+    }
+
+    public class Mitarbeiter : Person
+    {
+        public string Beruf { get; set; }
+    }
+
+    public class Abteilung
+    {
+        public int Id { get; set; }
+        public string Bezeichnung { get; set; }
+    }
+
+
+
+}
