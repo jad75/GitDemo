73da43cbd68bb7c70a11cac4ebddaf1c23344f77
André <andrerrr@outlook.de>
2019-07-10 14:39:12 +0200

    CodeFirst POCO Gen

---
+++ CodeFirstWithDb_POCOGen/CodeFirstWithDb_POCOGen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstWithDb_POCOGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** NorthWind ***");
+
+            using (var con = new MyDbContext("Data Source=.;Initial Catalog=Northwind;Integrated Security=True;"))
+            {
+                foreach (var e in con.Employees.ToList())
+                {
+                    Console.WriteLine($"{e.LastName}");
+                }
+            }
+
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+    }
+}
