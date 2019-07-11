1c333c90a9b8000c7eb336ad53a34dbdf02e97bb
André <andrerrr@outlook.de>
2019-07-11 10:48:12 +0200

    SpielerViewModel

---
+++ ppedv.VollE/ppedv.VollE.UI.WPF/ViewModels/SpielerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ppedv.VollE.Logic;
+using ppedv.VollE.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.VollE.UI.WPF.ViewModels
+{
+    public class SpielerViewModel
+    {
+        public ObservableCollection<Spieler> SpielerList { get; set; }
+
+        Core core = new Core();
+        public SpielerViewModel()
+        {
+            SpielerList = new ObservableCollection<Spieler>(core.Repository.GetAll<Spieler>());
+        }
+
+    }
+}
